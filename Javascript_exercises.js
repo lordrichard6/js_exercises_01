8e2c24870f1d490bb9dfa54d992e00c214f839b9
lordrichard <pauloreizinho@hotmail.com>
2019-06-26 19:24:41 +0200

    first commit

---
+++ Javascript_exercises.js
@@ -0,0 +1,8 @@
+// Write a function identity that takes an argument and returns that argument;
+const identify = function identify(x) {
+    return x
+}
+//or
+const identify = (x) => x;
+//---------------------------------------------------------------
+// Write a binary function addb that takes two numbers and returns their sum;
