578defa8de810e07554815449d371d2a2df03181
lordrichard <pauloreizinho@hotmail.com>
2019-06-26 19:47:37 +0200

    some exe added

--- Javascript_exercises.js
+++ Javascript_exercises.js
@@ -5,4 +5,32 @@ const identify = function identify(x) {
 //or
 const identify = (x) => x;
 //---------------------------------------------------------------
-// Write a binary function addb that takes two numbers and returns their sum;
+// Write a binary function add that takes two numbers and returns their sum;
+const add = (x, y) => {
+    return x + y;
+}
+console.log(add(4, 6));
+//---------------------------------------------------------------
+// Write a binary function sub that takes two numbers and returns their difference;
+const sub = (x, y) => {
+    return x - y;
+}
+console.log(sub(8, 6));
+//---------------------------------------------------------------
+// Write a binary function mult that takes two numbers and returns their product;
+const mult = (a, b) => {
+    return a * b;
+}
+console.log(mult(5, 4));
+//---------------------------------------------------------------
+// Write a binary function sml that takes two numbers and returns the smaller one;
+const sml = (a, b) => {
+    return a < b ? a : b;
+}
+console.log(sml(5, 7));
+//---------------------------------------------------------------
+// Write a binary function maxb that takes two numbers and returns the larger one;
+const lrg = (a, b) => {
+    return a > b ? a : b;
+}
+console.log(lrg(9, 4));
